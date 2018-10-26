d98367b9d1be4ca700b93573e7cb79b61459e2fc
Abraham Toriz <awonderfulcode@gmail.com>
2018-10-26 16:27:44 -0500

    basic REPL

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::io::{self, BufRead, Write};
+
 fn main() {
-    println!("Hello, world!");
+    let stdin = io::stdin();
+
+    loop {
+        print!("# ");
+        io::stdout().flush().ok().expect("Could not flush stdout");
+
+        let mut buf = String::new();
+
+        stdin.lock().read_line(&mut buf);
+
+        if buf.len() == 0 {
+            println!();
+            break;
+        }
+
+        println!("{:?} {}", buf, buf.len());
+    }
 }
