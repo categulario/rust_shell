dd3847026d0e9880dac3d8fbd5e4e6deca4d879b
Abraham Toriz <awonderfulcode@gmail.com>
2018-10-26 18:00:24 -0500

    restore functionality from split

--- src/parser.rs
+++ src/parser.rs
@@ -1,34 +1,72 @@
-pub fn parse(line: &String) -> Result<Vec<&str>, &str> {
-    return Ok(line.split_whitespace().collect());
+enum TokenType {
+    Word,
+    QuotedString,
+    Blank,
+    Or,
+    And,
+    Parenthesis,
+}
+
+pub fn parse(line: &String) -> Result<Vec<String>, &str> {
+    let mut tokens = Vec::new();
+    let mut cur_token = String::new();
+    let mut token_type = TokenType::Blank;
+
+    for c in line.chars() {
+        match token_type {
+            TokenType::Blank => {
+                if !c.is_whitespace() {
+                    token_type = TokenType::Word;
+                    cur_token.push(c);
+                }
+            }
+            TokenType::Word => {
+                if c.is_whitespace() {
+                    token_type = TokenType::Blank;
+                    tokens.push(cur_token.clone());
+                    cur_token = String::new();
+                } else if c.is_ascii_alphanumeric() {
+                    cur_token.push(c);
+                }
+            }
+            _ => {}
+        }
+    }
+
+    if cur_token.len() > 0 {
+        tokens.push(cur_token);
+    }
+
+    Ok(tokens)
 }
 
 #[test]
 fn test_parse_simple() {
     let line = String::from("ls -l");
 
-    assert_eq!(parse(&line), Ok(vec!["ls", "-l"]));
+    assert_eq!(parse(&line), Ok(vec!["ls".to_string(), "-l".to_string()]));
 }
 
-#[test]
-#[ignore]
-fn test_parse_double_quote() {
-    let line = String::from("echo \"hola mundo\"");
-
-    assert_eq!(parse(&line), Ok(vec!["echo", "\"hola mundo\""]));
-}
-
-#[test]
-#[ignore]
-fn test_parse_single_quote() {
-    let line = String::from("echo \'hola mundo\'");
-
-    assert_eq!(parse(&line), Ok(vec!["echo", "\'hola mundo\'"]));
-}
-
-#[test]
-#[ignore]
-fn test_parse_matches_quotes() {
-    let line = String::from("echo \"hola mundo\'");
-
-    assert_eq!(parse(&line), Err("Failed command line parsing"));
-}
+// 1#[test]
+// 1#[ignore]
+// 1fn test_parse_double_quote() {
+    // 1let line = String::from("echo \"hola mundo\"");
+// 1
+    // 1assert_eq!(parse(&line), Ok(vec!["echo", "\"hola mundo\""]));
+// 1}
+// 1
+// 1#[test]
+// 1#[ignore]
+// 1fn test_parse_single_quote() {
+    // 1let line = String::from("echo \'hola mundo\'");
+// 1
+    // 1assert_eq!(parse(&line), Ok(vec!["echo", "\'hola mundo\'"]));
+// 1}
+// 1
+// 1#[test]
+// 1#[ignore]
+// 1fn test_parse_matches_quotes() {
+    // 1let line = String::from("echo \"hola mundo\'");
+// 1
+    // 1assert_eq!(parse(&line), Err("Failed command line parsing"));
+// 1}
