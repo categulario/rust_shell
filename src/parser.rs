723871b8ec7c72e4488a69336ce93e65c65d1ceb
Abraham Toriz <awonderfulcode@gmail.com>
2018-10-26 17:07:23 -0500

    more tests and function signature

--- src/parser.rs
+++ src/parser.rs
@@ -1,10 +1,34 @@
-pub fn parse<'a>(line: &'a String) -> Vec<&'a str> {
-    return line.split_whitespace().collect();
+pub fn parse(line: &String) -> Result<Vec<&str>, &str> {
+    return Ok(line.split_whitespace().collect());
 }
 
 #[test]
 fn test_parse_simple() {
     let line = String::from("ls -l");
 
-    assert_eq!(parse(&line), ["ls", "-l"]);
+    assert_eq!(parse(&line), Ok(vec!["ls", "-l"]));
+}
+
+#[test]
+#[ignore]
+fn test_parse_double_quote() {
+    let line = String::from("echo \"hola mundo\"");
+
+    assert_eq!(parse(&line), Ok(vec!["echo", "\"hola mundo\""]));
+}
+
+#[test]
+#[ignore]
+fn test_parse_single_quote() {
+    let line = String::from("echo \'hola mundo\'");
+
+    assert_eq!(parse(&line), Ok(vec!["echo", "\'hola mundo\'"]));
+}
+
+#[test]
+#[ignore]
+fn test_parse_matches_quotes() {
+    let line = String::from("echo \"hola mundo\'");
+
+    assert_eq!(parse(&line), Err("Failed command line parsing"));
 }
