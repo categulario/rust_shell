20a524c8e913cfa214279f8328155009f1137dfe
vanessa <vanvanemily@gmail.com>
2018-10-27 10:15:47 -0500

    Adding a Struct for process.

--- src/process.rs
+++ src/process.rs
@@ -1,11 +1,20 @@
 use std::process::{Command, Stdio};
 
-pub fn receive_command(vector: &mut Vec<&str>)  {
-    let command  = vector.get(0).unwrap().to_string();
-    let getting_arguments: Vec<_> = vector.drain(1..).collect();
-    
-    if let Err(_) = Command::new(command)
-        .args(&getting_arguments)
+#[derive(Debug)]
+struct Process<'a> {
+    command: String,
+    arguments: Vec<&'a str>,
+}
+
+
+pub fn receive_command(vector: &mut Vec<&str>) {
+    let process = Process { 
+        command  : vector.get(0).unwrap().to_string(),
+        arguments : vector.drain(1..).collect(),
+    };
+
+    if let Err(_) = Command::new(process.command)
+        .args(process.arguments)
         .stdout(Stdio::inherit())
         .stderr(Stdio::inherit())
         .output() {
