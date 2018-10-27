997485b8c4b303a9e4d60107f875a70b7d3dc877
vanessa <vanvanemily@gmail.com>
2018-10-27 11:21:57 -0500

    Checking if the first element of the vector is cd and the following is a / to execute.

--- src/process.rs
+++ src/process.rs
@@ -1,11 +1,12 @@
 use std::process::{Command, Stdio};
+use std::env;
+use std::path::Path;
 
 #[derive(Debug)]
-struct Process<'a> {
+pub struct Process<'a> {
     command: String,
     arguments: Vec<&'a str>,
 }
-
 
 pub fn receive_command(vector: &mut Vec<&str>) {
     let process = Process { 
@@ -13,6 +14,17 @@ pub fn receive_command(vector: &mut Vec<&str>) {
         arguments : vector.drain(1..).collect(),
     };
 
+   if process.command == "cd"{
+       let cd = &process.arguments.get(0).unwrap().to_string();
+        execute_cd_process(cd.to_string());
+    }else if process.command == "exit"{
+       println!("Es exit");
+    }else {
+       execute_process(process);
+    }
+}
+
+pub fn execute_process(process: Process){
     if let Err(_) = Command::new(process.command)
         .args(process.arguments)
         .stdout(Stdio::inherit())
@@ -20,5 +32,17 @@ pub fn receive_command(vector: &mut Vec<&str>) {
         .output() {
             eprintln!("Command not found");
         }
+}
+
+pub fn execute_cd_process(cd: String){
+    let root = Path::new(&cd);
+    let _changed_dir = env::set_current_dir(&root).is_ok();
+    println!("The current directory is {}", root.display());
+}
+
+#[test]
+fn execute_a_single_command(){
+    let mut process = vec!["echo 'hello'"];
+    assert_eq!("hello".trim(), receive_command(&mut process) );
 }
 
